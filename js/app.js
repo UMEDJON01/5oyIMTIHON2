6e1368ab6b5a835c490ec36608b3bc82478e3a87
umedjon01 <najottalimfn15@gmail.com>
2024-04-07 10:33:23 +0500

    yangi kodlar

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
 const pElement = document.querySelector(".card__p");
 const button = document.querySelector(".card__btn");
 const h2Element = document.querySelector("h2");
+const container = document.querySelector(".container");
 
 function fetchData() {
   const apiURL = "https://api.adviceslip.com/advice";
   fetch(apiURL)
     .then((response) => response.json())
     .then((data) => {
-      pElement.textContent = data.slip.advice;
-      h2Element.textContent = `ADVICE      #${data.slip.id}`;
+      const advice = data.slip.advice;
+      const adviceId = data.slip.id;
+
+      pElement.textContent = advice;
+
+      h2Element.textContent = `ADVICE      #${adviceId}`;
+
+      const fontSize = calculateFontSize(advice);
+      pElement.style.fontSize = fontSize + "px";
+
+      adjustContainerHeight();
     })
     .catch((error) => console.error(error));
+}
+
+function calculateFontSize(text) {
+  const baseFontSize = 30;
+  const maxLength = 100;
+
+  return Math.max(baseFontSize - (text.length - maxLength) * 0.1, 16);
+}
+
+function adjustContainerHeight() {
+  const maxHeight = 500;
+  const currentHeight = container.offsetHeight;
+
+  if (currentHeight > maxHeight) {
+    container.style.height = maxHeight + "px";
+  }
 }
 
 button.addEventListener("click", fetchData);
